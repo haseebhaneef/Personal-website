b250d9e7dd72abcf3aa3e355ca65becefa07eba7
haseebhaneef <134126968+haseebhaneef@users.noreply.github.com>
2023-11-12 01:24:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-$('.po-button').mousedown(function(){
-    timeout = setInterval(function(){
-        window.scrollBy(0,-1); // May need to be -1 to go down
-    }, 0); // Play around with this number. May go too fast
 
-    return false;
-});
